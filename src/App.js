dc4318ac8c95b8d37ff647ab3d94c3b17106a85f
Moriah Humphries <moriah.humphries@gmail.com>
2020-08-16 16:21:18 -0500

    created functional component MyInfo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
 import './App.css';
 import MyApp from './Components/MyApp';
+import MyInfo from "./Components/MyInfo";
 
 class App extends Component {
     render() {
 
         return (
-            <MyApp />
+            <div>
+                <MyApp/>
+                <MyInfo/>
+            </div>
         );
     };
 }
---
+++ src/Components/MyInfo.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const MyInfo = () => {
+
+    return (
+        <div>
+            <h1>My name is Moriah</h1>
+            <p>I like cats</p>
+            <h2>My top 3 vacation spots</h2>
+            <ol>
+                <li>Utah</li>
+                <li>Dubai</li>
+                <li>Lake Tahoe</li>
+            </ol>
+        </div>
+    )
+}
+
+export default MyInfo;
