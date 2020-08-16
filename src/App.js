4cbc81b4fb7322d42cb25b59765c23fbc3405fab
Moriah Humphries <moriah.humphries@gmail.com>
2020-08-16 15:54:14 -0500

    Rendered list from component function

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React, {Component} from 'react';
 import './App.css';
+import MyApp from './Components/MyApp';
 
-class App extends React.Component {
+class App extends Component {
     render() {
+
         return (
-            <div>
-                <h1>React To-do List</h1>
-                <ul>
-                    <li>Cats</li>
-                    <li>Cats</li>
-                    <li>CATS</li>
-                </ul>
-            </div>
+            <MyApp />
         );
-    }
+    };
 }
 
 export default App;
---
+++ src/Components/MyApp.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const MyApp = () => {
+    return (
+    <div>
+        <h1>React To-do List</h1>
+        <ul>
+            <li>Cats</li>
+            <li>Cats</li>
+            <li>CATS</li>
+        </ul>
+    </div>
+    )
+};
+
+export default MyApp;
