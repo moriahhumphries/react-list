095a82adc602ec3db0073ef48be39986b406d3a2
Moriah Humphries <moriah.humphries@gmail.com>
2020-08-17 21:59:00 -0500

    added componentdidmount with api call

--- src/App.js
+++ src/App.js
@@ -9,31 +9,30 @@ class App extends React.Component {
     constructor() {
         super()
         this.state = {
-            todos: todosData
+            loading: false,
+            character: {}
         }
-        this.handleChange = this.handleChange.bind(this)
+        // this.handleChange = this.handleChange.bind(this)
     }
 
-    handleChange(id) {
-        this.setState(prevState => {
-            const updatedTodos = prevState.todos.map(todo => {
-                if (todo.id === id) {
-                    todo.completed = !todo.completed
-                }
-                return todo
+    componentDidMount() {
+        this.setState({loading: true})
+        fetch("https://pokeapi.co/api/v2/pokemon/1")
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    loading: false,
+                    character: data
+                })
             })
-            return {
-                todos: updatedTodos
-            }
-        })
     }
+
 
     render() {
-        const todoItems = this.state.todos.map(item => <TodoList key={item.id} item={item} handleChange={this.handleChange}/>)
-
+        const text = this.state.loading ? "Loading..." : this.state.character.name
         return (
-            <div className="todo-list">
-                {todoItems}
+            <div>
+                <p>{text}</p>
             </div>
         )
     }
